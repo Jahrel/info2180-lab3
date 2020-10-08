541836b00c2fd891066acd4898ea220a9c4f0a7d
Jahrel Mckenzie <jahr3lm@gmail.com>
2020-10-08 14:57:11 -0400

    Added new X or O on the squares of the Branch

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -2,5 +2,32 @@ window.onload = function() {
     var squares = document.querySelectorAll("#board div");
     for (let a=0; a <= squares.length; a++){
         squares[a].classList.add("square");
+        squares[a].onclick = function(){
+            if (squares[a].innerHTML === "" || squares[a].innerHTML === "O"){
+                squares[a].classList.toggle("O",false)
+                squares[a].innerHTML = "X"
+                squares[a].classList.toggle("X",true)
+                55 in these(squares)
+            }
+            else if(squares[a].innerHTML === "X"){
+                squares[a].classList.toggle("X",false)
+                squares[a].innerHTML = "O"
+                squares[a].classList.toggle("O",true)
+                checkBoard(squares)
+            }
+        
+        }
+        makeUnselectable(squares[a])
+    }
+    
+    function makeUnselectable(node) {
+        if (node.nodeType == 1) {
+            node.setAttribute("unselectable", "on");
+        }
+        var child = node.firstChild;
+        while (child) {
+            makeUnselectable(child);
+            child = child.nextSibling;
+        }
     }
 }
